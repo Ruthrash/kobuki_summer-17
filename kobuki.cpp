1c1444dac24d4cf870b2c7599a8b3e17db2d8d47
H Ruthrash <ruthrash.hari@gmail.com>
2017-05-16 14:35:01 +0530

    Add files via upload

--- kobuki.cpp
+++ kobuki.cpp
@@ -2,28 +2,45 @@
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <math.h>
+//#include <tgmath.h>
+//#include <tf/transform_broadcaster.h>
+//#include <Quartenion.h>
+#include <tf/tf.h>
 using namespace std;
 float xc = 0.0, yc = 0.0, tc = 0.0; //current pose
 float xg = 0.0, yg = 0.0, tg = 0.0; //goal points
 float v = 0.0, w = 0.0; //linear and angular velocities 
+float t_error = 0.0;
 
 void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg) // odometry subscriber callback
 {
 	xc = msg->pose.pose.position.x;
 	yc = msg->pose.pose.position.y;
+	double offset = 0;
 	//tc = get from quartenion;
+	/*tf::Matrix3x3 m(q);
+	double roll, pitch, yaw;
+	m.getRPY(roll, pitch, yaw);*/
+	geometry_msgs::Quaternion odom_quat = msg->pose.pose.orientation;
+	double yaw = tf::getYaw(odom_quat);
+	tc = yaw;
+	cout<<"The yaw is "<<yaw*180*7/22<<"\n";
 }
+
 void goalCallback(const geometry_msgs::Twist::ConstPtr& gmsg)// goal subcriber callback
 {
 	xg = gmsg->linear.x;
 	yg = gmsg->linear.y;
+	//cout<<"The goal is "<<atan((yg-yc)/(xg-xc))<<"\n";
 }
+
 float PID()// Calculate the angular velocity 
 { 
-  float t_error = 0.0, tIerror = 0.0, tDerror = 0.0, old_t_error = 0.0;	
-  tg = atan2((yg-yc)/(xg-xc));
+  float tIerror = 0.0, tDerror = 0.0, old_t_error = 0.0;	
+  tg = atan((yg-yc)/(xg-xc));
+  //cout<<"The goal is "<<tg<<"\n";
   t_error = tg - tc;
-  t_kp = 4; t_ki = 0.0; t_kd = 0.0;//setting PID gains
+  float t_kp = 4, t_ki = 0.0, t_kd = 0.0;//setting PID gains
   float  omega = 0.0; 
   tDerror = t_error - old_t_error;
   tIerror = tIerror + t_error;
@@ -46,10 +63,14 @@ int main(int argc, char **argv)
 
 	while(ros::ok()) //while ros is running properly
 	{
-		cout <<"The goal is "<<xg<<", "<<yg<<" Current postion is "<<xc<<", "<<yc<<"\n";
+		//cout <<"The goal is "<<xg<<", "<<yg<<" Current postion is "<<xc<<", "<<yc<<"\n";
 		geometry_msgs::Twist velocity; //a message object
-		v = 0.1;
-		w = PID();
+		v = 0.0;
+
+
+		//w = PID();
+		//cout<<"The omega is "<<w<<"\n";
+		//cout<<"The error is "<<t_error<<"\n";
 		velocity.linear.x = v;
 		velocity.angular.z = w;
 		chatter_pub.publish(velocity);
